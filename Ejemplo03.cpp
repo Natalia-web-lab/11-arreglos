1ba0d7d52d5123cafd0a74b55f45d68914460e8d
aquispeg <aquispeg@unjbg.edu.pe>
2025-07-09 22:07:19 -0500

    refactorizar codigo y agregar impresion del arreglo original

--- Ejemplo03.cpp
+++ Ejemplo03.cpp
@@ -1,31 +1,40 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int n;
-    int A[100], aux;
-
-    cout << "Ingrese la cantidad de elementos: ";
-    cin >> n;
-
-    for (int i = 0; i < n; i++) {
-        cout << "A[" << i << "]: ";
+void leerArreglo(int A[],int n){
+    for (int i=0;i<n;i++){
+        cout << " A[" << i << "]:  ";
         cin >> A[i];
     }
-
-    
+}
+void imprimirArreglo(int A[], int n){
+    for(int i=0;i<n;i++){
+        cout<<A[i]<<" ";
+    }
+    cout << endl;
+}
+void invertirArreglo(int A[],int n){
+    int aux;
     for (int i = 0; i < n / 2; i++) {
         aux = A[i];
         A[i] = A[n - 1 - i];
         A[n - 1 - i] = aux;
     }
+}
+int main() {
+    int n;
+    int A[100];
 
+    cout << "Ingrese la cantidad de elementos: ";
+    cin >> n;
+
+    leerArreglo(A,n);
+    cout<<"Arreglo original";
+    imprimirArreglo (A,n);
+    invertirArreglo(A,n);
+    cout<<"arreglo invertido";
+    imprimirArreglo (A,n);
     
-    cout << "\nArreglo resultante:\n";
-    for (int i = 0; i < n; i++) {
-        cout << A[i] << " ";
-    }
-
-    cout << endl;
+    
     return 0;
 }
